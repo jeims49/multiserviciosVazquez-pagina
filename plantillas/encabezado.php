32061f440586cdfe582ad41e737d85428552ab78
jeims49 <69992534+jeims49@users.noreply.github.com>
2022-04-29 12:19:57 -0500

    Add files via upload

---
+++ plantillas/encabezado.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Multiservicios Vazquez</title>
+    <link rel="stylesheet" href="estilos/style.css">
+    <link rel="shortcut icon" href="imagenes/palmera.ico" type="image/x-icon">
+    <link href="https://fonts.googleapis.com/css2?family=Assistant:wght@200&display=swap" rel="stylesheet">
+</head>
+<body> 
+        <!-- HEADER -->
+        <div class="header">
+            <h2>Contratista:</h2>
+            <h1> Jorge Cristían Vázquez López</h1>
+            <div class="aux_menu">
+                <ul class="menu" >
+                    <li><a href="index.php" ><b>Inicio</b></a></li>
+                    <li><a href="#"><b>Galeria de Trabajos</b></a> 
+                        <ul>
+                            <li class="sub"><a href="Galeria de Trabajos/albañileria.html"><b>Albañileria</b></a></li>
+                            <li class="sub"><a href="Galeria de Trabajos/pintura.html"><b>Pintura</b></a></li>
+                            <li class="sub"><a href="Galeria de Trabajos/electricidad.html"><b>Electricidad</b></a></li>
+                            <li class="sub"><a href="Galeria de Trabajos/Fontaneria.html"><b>Fontaneria</b></a></li>
+                        </ul>
+                    </li>
+                    <li><a href="recomendaciones.php"><b>Recomendaciones</b></a></li>
+                    <li> <a href="contactar.php"><b>Contactar</b></a></li>
+                </ul>
+            </div>
+        </div>
+            <!-- NAV -->
+            <div class="nav">
+                <img class="logo" src="imagenes/palmera.png" alt="logo">
+                <h3 class="titulo">Multiservicios Vázquez</h3>
+                <h3 class="sub">Especialista en Construcción y Remodelación</h3>
+            </div>
---
+++ plantillas/pie.php
@@ -0,0 +1,30 @@
+  <!-- FOOTER -->
+  <footer>
+                <p><b>Contacto Directo</b></p>
+                <div class="foot">
+                    
+                    <div class="num">
+                        <div class="colum">
+                            <img src="imagenes/iconos/telefono.ico">
+                            <label>315-102-3454</label>
+                        </div>
+                    </div>
+
+                    <div class="face">
+                        <div class="colum">
+                            <img src="imagenes/iconos/facebook.ico">
+                            <label>Jorge Vazquez</label>
+                        </div>
+                    </div>
+
+                    <div class="correo">
+                        <div class="colum">
+                            <img src="imagenes/iconos/correo.ico">
+                            <label>jorge321@gmal.com</label>
+                        </div>
+                    </div>
+                </div>
+
+            </footer>
+</body>
+</html>
